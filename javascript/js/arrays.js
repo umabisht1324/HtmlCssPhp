9e9919f67b4bcdf64eab97a0861c9233f5e462d7
Uma Bisht <anku.uma02@gmail.com>
2024-02-07 00:02:51 +0530

    littleAdvance

---
+++ javascript/js/arrays.js
@@ -0,0 +1,11 @@
+// Arrays
+/*
+arrays are the collects of items 
+*/
+
+let arr=["Uma","Anku","Mumma","Papa"];
+console.log(arr);
+
+arr.push("family");
+console.log(arr);
+console.log("abc".toUpperCase());
---
+++ javascript/js/methods.js
@@ -0,0 +1,23 @@
+// function are block of code that performs specific task and can be invoked whenever needed
+
+function myfunc()
+{
+    console.log("Wel Come");
+    console.log("Learning JS");
+}
+function func1(msg)
+{
+    console.log(msg);
+
+}
+
+function calculateSum(a,b)
+{
+    let sum=0;
+    sum=a+b;
+    return sum;
+}
+
+myfunc();
+func1("uma");
+console.log(calculateSum(5,4));
